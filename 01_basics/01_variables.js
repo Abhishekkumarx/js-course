2755e703228fa10de904d215c975c6ff899847a6
Abhishek Kumar <153820993+Abhishekkumarx@users.noreply.github.com>
2025-07-15 08:25:52 +0000

    updated

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 101700
+let email="ahsek@gmail.com" //use this for variables always in js
+var pass="111111" //issue in block scope and function scope
+city="bengha"
+
+let a
+
+// accountId=5050505 //can't be change
+
+email="golu@gmail.com"
+pass="33333333"
+city="nariyar"
+
+console.log("accountId")
+console.table([accountId,email,pass,city,a])
